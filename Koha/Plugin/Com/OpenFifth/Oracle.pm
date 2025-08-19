c7a23a712eeb02c5982814598de03f8c12891049
Martin Renvoize <martin.renvoize@openfifth.co.uk>
2025-08-19 13:40:30 +0100

    chore: bump version

--- Koha/Plugin/Com/OpenFifth/Oracle.pm
+++ Koha/Plugin/Com/OpenFifth/Oracle.pm
@@ -12,7 +12,7 @@ use List::Util qw(min max);
 use Mojo::JSON qw{ decode_json };
 use Text::CSV;
 
-our $VERSION = '0.0.30';
+our $VERSION = '0.0.31';
 
 our $metadata = {
     name => 'Oracle Finance Integration',
